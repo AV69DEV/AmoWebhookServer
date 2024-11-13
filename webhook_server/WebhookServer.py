0745782c2b3afc41b942516fd7797a57142c7c98
AV69DEV <av69.dev@gmail.com>
2024-11-13 09:47:21 +0100

    New flask instance for receiving webhooks

---
+++ webhook_server/WebhookServer.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+app = Flask(__name__, static_folder=os.getenv('STATIC_FOLDER'),
+            template_folder=os.getenv('TEMPLATE_FOLDER'))
+
+
+@app.route('/webhooks', methods=['POST'])
+def receive_webhook():
+    webhook_data = request.form.to_dict()
+    print("Получен вебхук:", webhook_data)
+    return "Webhook received", 200
+
+
+if __name__ == '__main__':
+    app.run(port=3002)
